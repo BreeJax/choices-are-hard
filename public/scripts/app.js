9a8f9f40cd7f37557b8654e4e8500242168a0ae4
Bree Jackson <bjacksonsisterwolf@gmail.com>
2018-01-25 03:51:05 -0500

    visability changed and updated

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,62 +8,31 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-console.log("Hey Bree");
+console.log("Hello there beautiful!");
 
-var CounterApp = function (_React$Component) {
-  _inherits(CounterApp, _React$Component);
+var Visable = function (_React$Component) {
+  _inherits(Visable, _React$Component);
 
-  function CounterApp(props) {
-    _classCallCheck(this, CounterApp);
+  function Visable(props) {
+    _classCallCheck(this, Visable);
 
-    var _this = _possibleConstructorReturn(this, (CounterApp.__proto__ || Object.getPrototypeOf(CounterApp)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (Visable.__proto__ || Object.getPrototypeOf(Visable)).call(this, props));
 
-    _this.addOne = _this.addOne.bind(_this);
-    _this.minusOne = _this.minusOne.bind(_this);
-    _this.resetCounter = _this.resetCounter.bind(_this);
+    _this.hidden = _this.hidden.bind(_this);
     _this.state = {
-      count: 0
+      showing: false
     };
     return _this;
   }
 
-  _createClass(CounterApp, [{
-    key: "minusOne",
-    value: function minusOne() {
+  _createClass(Visable, [{
+    key: "hidden",
+    value: function hidden() {
       this.setState(function (prevState) {
         return {
-          count: prevState.count - 1
+          showing: !prevState.showing
         };
       });
-    }
-  }, {
-    key: "addOne",
-    value: function addOne() {
-      this.setState(function (prevState) {
-        return {
-          count: prevState.count + 1
-        };
-      });
-    }
-  }, {
-    key: "resetCounter",
-    value: function resetCounter() {
-      this.setState(function () {
-        return {
-          count: 0
-        };
-      });
-      this.setState(function (prevState) {
-        return {
-          count: prevState.count + 1
-        };
-      });
-      // this.setState({
-      //   count: 0
-      // })
-      // this.setState({
-      //   count: this.state.count + 1
-      // }) <= this makes it so that it doens't render correctly- adding one rather then simply setting it to 0, or 1 as the above code does. Passing an object is probably never going to be used again.
     }
   }, {
     key: "render",
@@ -74,60 +43,60 @@ var CounterApp = function (_React$Component) {
         React.createElement(
           "h1",
           null,
-          "Count: ",
-          this.state.count
+          "Harry Potter!"
+        ),
+        React.createElement(
+          "h3",
+          null,
+          "Do you have your cloke? "
         ),
         React.createElement(
           "button",
-          { onClick: this.addOne },
-          "+1"
+          { onClick: this.hidden },
+          this.state.showing ? "No, I don't" : "Yes!! I have it!"
         ),
-        React.createElement(
-          "button",
-          { onClick: this.minusOne },
-          "-1"
-        ),
-        React.createElement(
-          "button",
-          { onClick: this.resetCounter },
-          "reset"
+        this.state.showing && React.createElement(
+          "div",
+          null,
+          React.createElement(
+            "p",
+            null,
+            "Hurry to the restricted section, Mr. Potter!"
+          )
         )
       );
     }
   }]);
 
-  return CounterApp;
+  return Visable;
 }(React.Component);
 
-ReactDOM.render(React.createElement(CounterApp, null), document.getElementById("app"));
+ReactDOM.render(React.createElement(Visable, null), document.getElementById("app"));
 
-// let count = 0
-// let addOne = () => {
-//   count++
-//   //count = count + 1
-//   renderCounterApp()
-// }
-// let minusOne = () => {
-//   count--
-//   //count = count - 1
-//   renderCounterApp()
-// }
-// let reset = () => {
-//   count = 0
-//   renderCounterApp()
+// const appRoot = document.getElementById("app")
+//
+// let showing = false
+//
+// let hidden = () => {
+//   showing = !showing
+//   console.log(showing)
+//   visable()
 // }
 //
-// const renderCounterApp = () => {
-//   const templateTwo = (
+// const visable = () => {
+//   let template = (
 //     <div>
-//       <h1>Count: {count}</h1>
-//       <button onClick={addOne}>+1</button>
-//       <button onClick={minusOne}>-1</button>
-//       <button onClick={reset}>reset</button>
+//       <h1>Harry Potter!</h1>
+//   <h3>Do you have your cloke?</h3>
+//   <button onClick={hidden}>{showing ? "No, I don't" : "Yes!! I have it!"}</button>
+//   {/* {showing === false ? "Hurry to the restricted section, Mr. Potter!" : ""} */}
+//   {showing && (
+//     <div>
+//       <p>Hurry to the restricted section, Mr. Potter!</p>
 //     </div>
+//   )}
+// </div>
 //   )
-//
-//   ReactDOM.render(templateTwo, appRoot)
+//   ReactDOM.render(template, appRoot)
 // }
-//
-// renderCounterApp()
+// visable()
--- src/playground/build-it-visable.js
+++ src/playground/build-it-visable.js
@@ -1,29 +1,61 @@
 console.log("Hello there beautiful!")
 
-const appRoot = document.getElementById("app")
-
-let showing = false
-
-let hidden = () => {
-  showing = !showing
-  console.log(showing)
-  visable()
+class Visable extends React.Component {
+  constructor(props) {
+    super(props)
+    this.hidden = this.hidden.bind(this)
+    this.state = {
+      showing: false
+    }
+  }
+  hidden() {
+    this.setState((prevState) => {
+      return {
+        showing: !prevState.showing
+      }
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h1>Harry Potter!</h1>
+        <h3>Do you have your cloke? </h3>
+        <button onClick={this.hidden}>{this.state.showing ? "No, I don't" : "Yes!! I have it!"}</button>
+        {this.state.showing && (
+          <div>
+            <p>Hurry to the restricted section, Mr. Potter!</p>
+          </div>
+        )}
+      </div>
+    )
+  }
 }
+ReactDOM.render(<Visable />, document.getElementById("app"))
 
-const visable = () => {
-  let template = (
-    <div>
-      <h1>Harry Potter!</h1>
-      <h3>Do you have your cloke?</h3>
-      <button onClick={hidden}>{showing ? "No, I don't" : "Yes!! I have it!"}</button>
-      {/* {showing === false ? "Hurry to the restricted section, Mr. Potter!" : ""} */}
-      {showing && (
-        <div>
-          <p>Hurry to the restricted section, Mr. Potter!</p>
-        </div>
-      )}
-    </div>
-  )
-  ReactDOM.render(template, appRoot)
-}
-visable()
+// const appRoot = document.getElementById("app")
+//
+// let showing = false
+//
+// let hidden = () => {
+//   showing = !showing
+//   console.log(showing)
+//   visable()
+// }
+//
+// const visable = () => {
+//   let template = (
+//     <div>
+//       <h1>Harry Potter!</h1>
+//   <h3>Do you have your cloke?</h3>
+//   <button onClick={hidden}>{showing ? "No, I don't" : "Yes!! I have it!"}</button>
+//   {/* {showing === false ? "Hurry to the restricted section, Mr. Potter!" : ""} */}
+//   {showing && (
+//     <div>
+//       <p>Hurry to the restricted section, Mr. Potter!</p>
+//     </div>
+//   )}
+// </div>
+//   )
+//   ReactDOM.render(template, appRoot)
+// }
+// visable()
