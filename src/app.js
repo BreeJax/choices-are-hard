31aaeb43fc00cc4a2c1a7db675022d407e5cb271
Bree Jackson <bjacksonsisterwolf@gmail.com>
2018-02-27 18:10:43 -0500

    Getting around to styling. I might have to make a custom script to be able to make this for myself.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import ChoicesAreHardApp from "./components/ChoicesAreHardApp"
+import "./styles/styles.scss"
 
 ReactDOM.render(<ChoicesAreHardApp />, document.getElementById("app"))
