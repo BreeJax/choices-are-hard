a69c503caccd4503687a8bd80c409f214af169c5
Bree Jackson <bjacksonsisterwolf@gmail.com>
2018-02-27 17:56:48 -0500

    Took {} away from some of them- frankly, I like the return, it's not hurting anything, so I'm going to try and keep it. I think it helps me understand in the long run what everything is doing, and one word never hurt anyone.

--- src/components/Options.js
+++ src/components/Options.js
@@ -2,16 +2,12 @@ import React from "react"
 import Option from "./Option"
 
 //Stateless functional Component
-const Options = (props) => {
-  return (
-    <div>
-      <button onClick={props.handleDeleteOptions}>Remove All Options</button>
-      {props.options.length === 0 && <p>Add something to get started!</p>}
-      {props.options.map((option) => (
-        <Option key={option} optionText={option} handleDeleteOne={props.handleDeleteOne} />
-      ))}
-    </div>
-  )
-}
+const Options = (props) => (
+  <div>
+    <button onClick={props.handleDeleteOptions}>Remove All Options</button>
+    {props.options.length === 0 && <p>Add something to get started!</p>}
+    {props.options.map((option) => <Option key={option} optionText={option} handleDeleteOne={props.handleDeleteOne} />)}
+  </div>
+)
 
 export default Options
